dc15a7ce2adaca870d7d55cb52171e7416f07870
HARI KRISHNAN CK <ph1160562@iitd.ac.in>
2019-06-10 19:57:38 +0530

    Delete serializers.py

--- serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Activate
-class activateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Activate
-        fields = '__all__'
